74a9fec2de1de5323c5dee6ec00cf15d46a35510
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-09 00:51:49 -0500

    added app.js logic and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CharCard from "./components/CharCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./matchcards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click each member of the hit BBC drama Peaky Blinders. But don't click anyone more than once, or you lose!";
+
+class App extends Component {
+    
+    // Setting this.state.matches to the matches json array
+    state = {
+        matches,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
+
+    setClicked = id => {
+
+        // Make a copy of the state matches array to work with
+        const matches = this.state.matches;
+
+        // Filter for the clicked match
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        // If the matched image's clicked value is already true, 
+        // do the game over actions
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct: " + correctGuesses);
+            console.log ("High Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "This character has already been picked, you lose!"
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        // Otherwise, if clicked = false, and the user hasn't finished
+        } else if (correctGuesses < 11) {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // increment the appropriate counter
+            correctGuesses++;
+            
+            clickMessage = "Good job, this is a new character!";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // restart the guess counter
+            correctGuesses = 0;
+
+            // Egg on the user to play again
+            clickMessage = "You've clicked on every character!";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>Peaky Blinders Memory Click Game</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary card-header">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+                <div className="container">
+                <div className="row">
+                {this.state.matches.map(match => (
+                    <CharCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+                </div>
+                </div>
+
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
