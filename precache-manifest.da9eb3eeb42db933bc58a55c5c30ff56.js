eb9a6d3bec5ed796855c0de4b97f99ef9545b622
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-08 23:30:31 -0500

    Updates

---
+++ precache-manifest.da9eb3eeb42db933bc58a55c5c30ff56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715e55d382a82d1a95a896e3f9a5ab03",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d660477ddbd9e55d7513",
+    "url": "/clicky-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6effbca24685ced80aa",
+    "url": "/clicky-game/static/js/2.61f2fa50.chunk.js"
+  },
+  {
+    "revision": "d660477ddbd9e55d7513",
+    "url": "/clicky-game/static/js/main.f0c327ae.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clicky-game/static/media/logo.25bf045c.svg"
+  }
+]);
